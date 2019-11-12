8b73cba9b2f46703edee63fcbff7335feec47b8d
Cristo Daniel Navarro Rodriguez <alu0101024608@ull.edu.es>
2019-11-12 19:07:00 +0000

    Comprobando que se puede instanciar un alimento

--- lib/Practica6/alimento.rb
+++ lib/Practica6/alimento.rb
@@ -1,2 +1,5 @@
 class Alimento
+	def initialize (a,b,c,d,e,f)
+                @nombre,@proteinas,@carbohidratos,@lipidos,@gases,@terreno = a,b,c,d,e,f
+        end
 end
--- spec/Practica6_spec.rb
+++ spec/Practica6_spec.rb
@@ -1,2 +1,8 @@
 RSpec.describe Alimento do
+
+	context "Probando a instanciar un alimento, acceso a variables y to_s: " do
+                it "Se puede instanciar un alimento." do
+                        a = Alimento.new("Carne de vaca", 21.1, 0.0, 3.1, 50.0, 164.0)
+		end
+        end
 end
