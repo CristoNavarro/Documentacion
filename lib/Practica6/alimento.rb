f711fdafe8f39f3b84ad9bf87e00aa730fa6f189
Cristo Daniel Navarro Rodriguez <alu0101024608@ull.edu.es>
2019-11-12 19:12:57 +0000

    Comprobando que los getter funcionan/existen las variables y se pueden obtener sus valores

--- lib/Practica6/alimento.rb
+++ lib/Practica6/alimento.rb
@@ -1,4 +1,6 @@
 class Alimento
+	attr_reader :nombre, :proteinas, :carbohidratos, :lipidos, :gases, :terreno
+
 	def initialize (a,b,c,d,e,f)
                 @nombre,@proteinas,@carbohidratos,@lipidos,@gases,@terreno = a,b,c,d,e,f
         end
--- spec/Practica6_spec.rb
+++ spec/Practica6_spec.rb
@@ -1,8 +1,29 @@
 RSpec.describe Alimento do
 
+	before (:all) do
+                @chocolate = Alimento.new("Chocolate", 5.3, 47.0, 30.0, 2.3, 3.4)
+                @nuez = Alimento.new("Nuez", 20.0, 21.0, 54.0, 0.3, 7.9)
+        end
+
 	context "Probando a instanciar un alimento, acceso a variables y to_s: " do
                 it "Se puede instanciar un alimento." do
                         a = Alimento.new("Carne de vaca", 21.1, 0.0, 3.1, 50.0, 164.0)
 		end
+
+		it "Los getter funcionan." do
+                        expect(@chocolate.nombre).to eq("Chocolate")
+                        expect(@chocolate.proteinas).to eq(5.3)
+                        expect(@chocolate.carbohidratos).to eq(47.0)
+                        expect(@chocolate.lipidos).to eq(30.0)
+                        expect(@chocolate.gases).to eq(2.3)
+                        expect(@chocolate.terreno).to eq(3.4)
+                        expect(@nuez.nombre).to eq("Nuez")
+                        expect(@nuez.proteinas).to eq(20.0)
+                        expect(@nuez.carbohidratos).to eq(21.0)
+                        expect(@nuez.lipidos).to eq(54.0)
+	                expect(@nuez.gases).to eq(0.3)
+	                expect(@nuez.terreno).to eq(7.9)
+                end
+
         end
 end
