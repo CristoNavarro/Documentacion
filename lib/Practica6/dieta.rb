c4e73124aedf40121f37f35f7c556c5dd139e533
Cristo Daniel Navarro Rodriguez <alu0101024608@ull.edu.es>
2019-12-19 19:58:17 +0000

    Clase dieta documentada

--- lib/Practica6/dieta.rb
+++ lib/Practica6/dieta.rb
@@ -1,9 +1,25 @@
-class Dieta
+# Autor:: Cristo Daniel Navarro Rodriguez
+#
+# == Clase Dieta
+#
+# Esta clase fue creada en las primeras practicas para representar una dieta (conjunto de alimentos).
 
+class Dieta
+	
+	# Array de alimentos de la dieta
+	attr_reader :alimentos
+
+	# Inicializa la dieta
+	#
+	# ==== Parametros
+	#
+	# * +array+ - Array de alimentos
 
 	def initialize (array)
 		@alimentos = array
 	end
+
+	# Calcula el impacto ambiental que tiene la dieta formateada como un array +[gases,terreno]+
 
 	def impacto 
 		resultado = [0,0]
@@ -11,11 +27,15 @@ class Dieta
 		return resultado
 	end
 
+	# Calcula las kcal aportadas por la dieta
+
 	def energia
 		resultado = 0;
 		@alimentos.each {|i| resultado = resultado + i.kcal_total}
 		return resultado
 	end
+
+	# Calcula las proteinas del conjunto de alimentos
 
 	def prot
 		resultado = 0
